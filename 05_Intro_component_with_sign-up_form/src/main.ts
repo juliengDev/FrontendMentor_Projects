3991d2af11d8f8a6b8185e4a2ef2b8f8a66ddf3d
Julien Gilbert <juliengilbert.dev@gmail.com>
2025-07-01 18:37:13 +0700

    project 05 done

--- 05_Intro_component_with_sign-up_form/src/main.ts
+++ 05_Intro_component_with_sign-up_form/src/main.ts
@@ -1,48 +1,106 @@
 import "./style.css";
 import * as z from "zod";
 
-const EmailSchema = z.object({
+const errorMessage = {
+  minLengthErrorMessage: "Password must be at least 8 characters long.",
+  maxLengthErrorMessage: "Password must not exceed 20 characters.",
+  uppercaseErrorMessage: "Password must include at least one uppercase letter.",
+  lowercaseErrorMessage: "Password must include at least one lowercase letter.",
+  numberErrorMessage: "Password must include at least one number.",
+  specialCharacterErrorMessage:
+    "Password must include at least one special character (e.g. !@#$%).",
+};
+
+const FormSchema = z.object({
+  firstname: z.string().min(1, "Firstname required"),
+  lastname: z.string().min(1, "Lastname required"),
   email: z
     .string()
     .min(1, "Email required")
     .email("Please provide a valid email"),
+  password: z
+    .string()
+    .min(8, { message: errorMessage.minLengthErrorMessage })
+    .max(20, { message: errorMessage.maxLengthErrorMessage })
+    .refine((password) => /[A-Z]/.test(password), {
+      message: errorMessage.uppercaseErrorMessage,
+    })
+    .refine((password) => /[a-z]/.test(password), {
+      message: errorMessage.lowercaseErrorMessage,
+    })
+    .refine((password) => /[0-9]/.test(password), {
+      message: errorMessage.numberErrorMessage,
+    })
+    .refine((password) => /[!@#$%^&*(),.?":{}|<>]/.test(password), {
+      message: errorMessage.specialCharacterErrorMessage,
+    }),
 });
 
 const formEl = document.getElementById("form") as HTMLFormElement;
-const emailInputEl = document.getElementById("email") as HTMLInputElement;
-const errorEl = document.querySelector(".error-msg") as HTMLParagraphElement;
-const errorIcon = document.querySelector(".error-icon") as HTMLImageElement;
 
-function displayError(message: string): void {
-  errorEl.textContent = message;
-  errorEl.className = "self-start pl-6 text-[#F96464]";
-  errorIcon.className = "absolute top-1/2 right-20 -translate-y-1/2";
-  emailInputEl.classList.add("border-2", "border-[#F96464]");
-  emailInputEl.classList.remove("border-base-pink-primary", "border-1");
-}
+const showError = (fieldName: string, message: string) => {
+  const input = document.getElementById(fieldName);
+  if (!input) return;
+  input.classList.add("border-red-500", "border-2");
+  input.setAttribute("aria-invalid", "true");
 
-function clearError(): void {
-  errorEl.textContent = "";
-  errorEl.className = "";
-  errorIcon.className = "";
-  emailInputEl.classList.add("border-base-pink-primary", "border-1");
-  emailInputEl.classList.remove("border-2 border-[#F96464]");
-}
+  const errorElement = input.parentElement?.querySelector(".error");
+  const imgErrorElement = input.parentElement?.querySelector("img");
+  if (errorElement) {
+    errorElement.textContent = message;
+    errorElement.classList.add(
+      "text-base-pink",
+      "mt-[6px]",
+      "text-end",
+      "italic",
+    );
+    imgErrorElement?.classList.remove("hidden");
+    errorElement.setAttribute("role", "alert");
+  }
+};
 
-formEl.addEventListener("submit", (e: Event) => {
+const clearErrors = () => {
+  document.querySelectorAll(".error").forEach((el) => {
+    el.textContent = "";
+    el.classList.remove("text-base-pink", "mt-[6px]", "text-end", "italic");
+  });
+  document.querySelectorAll("input").forEach((input) => {
+    input.classList.remove("border-red-500", "border-2");
+    input.removeAttribute("aria-invalid");
+    input.value = "";
+  });
+  document
+    .querySelectorAll("img")
+    .forEach((img) => img.classList.add("hidden"));
+};
+
+formEl.addEventListener("submit", function (e: Event) {
   e.preventDefault();
 
   const target = e.target as HTMLFormElement;
   const formData = new FormData(target);
-  const email = formData.get("email") as string;
+  const user = {
+    firstname: (formData.get("firstname") as string)?.trim() || "",
+    lastname: (formData.get("lastname") as string)?.trim() || "",
+    email: (formData.get("email") as string)?.trim().toLowerCase() || "",
+    password: (formData.get("password") as string)?.trim() || "",
+  };
 
-  const result = EmailSchema.safeParse({ email });
+  const result = FormSchema.safeParse(user);
 
   if (!result.success) {
-    const firstError = result.error.issues[0];
-    displayError(firstError.message);
+    const fieldErrors = result.error.flatten().fieldErrors;
+
+    (Object.keys(fieldErrors) as Array<keyof typeof fieldErrors>).forEach(
+      (key) => {
+        const message = fieldErrors[key]?.[0];
+        if (message) {
+          showError(key as string, message);
+        }
+      },
+    );
   } else {
-    clearError();
-    console.log("Email valide:", result.data);
+    clearErrors();
+    console.log("✅ Valid data :", result.data);
   }
 });
