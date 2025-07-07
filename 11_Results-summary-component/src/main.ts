6ee1dcbf05c31bd4355dd80baaab5e8e2ed2f6c1
Julien Gilbert <juliengilbert.dev@gmail.com>
2025-07-07 19:01:13 +0200

    fixing colors logic

--- 11_Results-summary-component/src/main.ts
+++ 11_Results-summary-component/src/main.ts
@@ -1,4 +1,33 @@
 import "./style.css";
 import data from "../data.json";
 
-console.log(data);
+const infoGrpEl = document.getElementById("grp") as HTMLDivElement;
+
+data.forEach((el) => {
+  const { category, score, icon, color } = el;
+  const component = `
+  <div class="bg-base-${color}-50 flex items-center justify-between rounded-xl px-5 py-4">
+    <div class="flex gap-4">
+      <img src=${icon} alt="icon reaction" />
+      <p
+        class="text-base-${color}-400 text-base leading-[130%] font-medium"
+      >
+        ${category}
+      </p>
+    </div>
+    <div>
+      <p
+        class="text-base-${color}-950/50 text-base leading-[130%] font-bold"
+      >
+        <span class="text-base-${color}-950">${score}</span> / 100
+      </p>
+    </div>
+  </div>
+  `;
+  const temp = document.createElement("div");
+  temp.innerHTML = component.trim();
+  const element = temp.firstElementChild;
+  if (element) {
+    infoGrpEl.insertAdjacentElement("beforeend", element);
+  }
+});
