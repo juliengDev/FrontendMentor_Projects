dd70e24af9fbd6601f9ff830691870b90fba112c
Julien Gilbert <juliengilbert.dev@gmail.com>
2025-07-18 17:37:43 +0200

    adding new section

---
+++ junior/22_Blogr-landing-page/vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  plugins: [tailwindcss()],
+  base: "/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        // location: resolve(__dirname, "pages/location.html"),
+      },
+    },
+  },
+});
