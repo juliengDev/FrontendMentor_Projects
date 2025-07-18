80b718681ff359bb6376b3b5afaa15d99fc4ac53
Julien Gilbert <juliengilbert.dev@gmail.com>
2025-07-18 10:34:59 +0200

    fixing navbar && two section done

--- junior/23_Loopstudios_landing_page/src/scripts/menu.ts
+++ junior/23_Loopstudios_landing_page/src/scripts/menu.ts
@@ -4,14 +4,12 @@ const iconClose = document.getElementById("icon-close") as HTMLButtonElement;
 
 function openMenu() {
   menu.classList.remove("hidden");
-  menu.classList.add("open");
   menu.classList.add("flex");
   iconBurger.classList.add("hidden");
   iconClose.classList.remove("hidden");
 }
 function closeMenu() {
   menu.classList.add("hidden");
-  menu.classList.remove("open");
   menu.classList.remove("flex");
   iconBurger.classList.remove("hidden");
   iconClose.classList.add("hidden");
@@ -20,7 +18,6 @@ function closeMenu() {
 iconBurger.addEventListener("click", openMenu);
 iconClose.addEventListener("click", closeMenu);
 
-// Optionnel : fermer le menu si on clique en dehors
 document.addEventListener("click", (e) => {
   const target = e.target as Node;
   const isClickInside = iconBurger.contains(target) || menu.contains(target);
