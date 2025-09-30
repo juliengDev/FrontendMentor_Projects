7ff5c613131705fdbc45dcd66a555a6a85300d67
Julien Gilbert <juliengilbert.dev@gmail.com>
2025-09-30 16:59:03 +0200

    setting up project + system design added

--- junior/33_Product-list-with-cart-main/frontend/src/components/RecipeList.tsx
+++ junior/33_Product-list-with-cart-main/frontend/src/components/RecipeList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 // Définissons une interface pour la structure d'une recette
 // Cela nous apportera la sécurité de typage avec TypeScript
@@ -21,7 +21,7 @@ interface RecipeListProps {
 const RecipeList: React.FC<RecipeListProps> = ({ recipes }) => {
   return (
     <div>
-      <h2>Nos recettes</h2>
+      <h2>Desserts</h2>
       <ul>
         {recipes.map((recipe, index) => (
           <li key={index}>{recipe.name}</li>
